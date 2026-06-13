260645b390fe9345bba00e5f21d999addaa40bb9
zect <timofejsuslin98@gmail.com>
2026-06-13 22:06:34 +0700

    Update main.py

--- web/main.py
+++ web/main.py
@@ -1,41 +1,37 @@
 from fastapi import FastAPI, Form, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
-
 from catboost import CatBoostRegressor
 from pathlib import Path
 import pandas as pd
 import numpy as np
 import joblib
 
-# ===  НАСТРОЙКА ПУТЕЙ  ===
-BASE_DIR = Path(__file__).resolve().parent.parent  # корень проекта 
 
+# ===  НАСТРОЙКА ПУТЕЙ  ===
+BASE_DIR = Path(__file__).resolve().parent.parent  
 MODEL_PATH = BASE_DIR / "models" / "kvartis_model.cbm"
 SCALER_PATH = BASE_DIR / "models" / "kvartis_scaler.pkl"
-
 
 # ===  ЗАГРУЗКА МОДЕЛИ И СКАЛЕРА  ===
 model = CatBoostRegressor()
 model.load_model(str(MODEL_PATH))
-
 scaler = joblib.load(str(SCALER_PATH))
 
 app = FastAPI(title="Kvartis Price Predictor")
 templates = Jinja2Templates(directory=str(BASE_DIR / "web" / "templates"))
 
-
 @app.get("/", response_class=HTMLResponse)
 async def home(request: Request):
+
     return templates.TemplateResponse(
-        "index.html",
-        {
-            "request": request,
+        request, 
+        name="index.html",
+        context={
             "result": None,
             "inputs": {}
         }
     )
-
 
 @app.post("/", response_class=HTMLResponse)
 async def predict(
@@ -50,11 +46,8 @@ async def predict(
     floor: int = Form(...),
     all_floor: int = Form(...)
 ):
-
-
     current_year = 2026  
     building_age = current_year - year
-
 
     data = {
         'city': [city],
@@ -70,17 +63,17 @@ async def predict(
     x = pd.DataFrame(data)
 
     # ===  Масштабирование числовых признаков  ===
-
     numeric = ['rooms', 'm2', 'kitchen_m2', 'building_age', 'floor', 'all_floor']
     x[numeric] = scaler.transform(x[numeric])
 
     # ===  Предсказание  ===
+
     pred_log = model.predict(x)
     pred_price = np.expm1(pred_log)[0]
 
-    # Вывод в консоль для отладки
+    # Вывод в консоль
     print(x.to_string(index=False))
-    print("цена ≈", f"{pred_price:,.0f}", "₽")
+    print("цена ≈ ", f"{pred_price:,.0f} ", "₽ ")
 
     result = f"Цена ≈ {pred_price:,.0f} ₽"
 
@@ -98,9 +91,9 @@ async def predict(
     }
 
     return templates.TemplateResponse(
-        "index.html",
-        {
-            "request": request,
+        request, 
+        name="index.html",
+        context={
             "result": result,
             "inputs": inputs
         }
